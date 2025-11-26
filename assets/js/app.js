ef031abb6ed1a3ca7709cbc10c3143f389361f79
tmombekov029-oss <tmombekov029@gmail.com>
2025-11-26 09:45:12 +0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function openMenu(){
+    document.getElementById("sidebar").style.left = "0";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeMenu(){
+    document.getElementById("sidebar").style.left = "-250px";
+    document.getElementById("overlay").style.display = "none";
+}
